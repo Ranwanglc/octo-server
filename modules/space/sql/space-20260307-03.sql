cd45cc5e6a2170e8e9306bc3bf7542a2a894aa19
yujiawei <yujiawei-1c1bbaa98d2b@users.noreply.github.com>
2026-03-07 16:21:31 +0800

    fix: add missing -- +migrate Up header to space migration (#222)
    Co-authored-by: DevBot <devbot@dmwork.org>

--- modules/space/sql/space-20260307-03.sql
+++ modules/space/sql/space-20260307-03.sql
@@ -1,3 +1,4 @@
+-- +migrate Up
 ALTER TABLE `conversation` ADD COLUMN `space_id` VARCHAR(40) DEFAULT '' COMMENT 'Space ID，空字符串表示个人空间';
 CREATE INDEX `idx_conversation_space` ON `conversation`(`uid`, `space_id`);
 ALTER TABLE `group` ADD COLUMN `space_id` VARCHAR(40) DEFAULT '' COMMENT 'Space ID';
